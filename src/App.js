1ff014d26dc928beda0d065106cde9ef0341c4be
Martin Shell <martin@osiopt.com>
2019-12-11 14:45:12 -0500

    add header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 import { Route, Switch } from "react-router-dom";
 
@@ -13,9 +14,10 @@ import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
