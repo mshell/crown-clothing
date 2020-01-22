333d4df278af60078af33c4ae30d2c16fda7cb5f
Martin Shell <martin@osiopt.com>
2020-01-22 15:34:18 -0500

    fixed missing file

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,8 +6,9 @@ import {
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
 import "./checkout.styles.scss";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
-import  CheckoutItem  from "../../components/checkout-item/checkout-item.component";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -31,9 +32,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total"></div>
+    <span>TOTAL: ${total}</span>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 -- Exp: 1/20 -- CVV: 123
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
