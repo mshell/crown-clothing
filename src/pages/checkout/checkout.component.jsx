09d51e6c5ea257a25a995f4a919607848cba4194
Martin Shell <martin@osiopt.com>
2020-01-22 12:38:21 -0500

    moved our directory and shop data into its own reducer, created respective redux flow for shop/component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -32,7 +32,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>${total}</span>
+      <span>TOTAL: ${total}</span>
     </div>
   </div>
 );
